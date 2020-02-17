876451f365f3f1031e83b4027f833365f8325c4d
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-17 13:46:01 +0700

    project laravel bersama batch 3

---
+++ resources/views/includes/checkout/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+    <nav class="row navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
+            <a class="navbar-brand" href="index.html">
+                <img src="frontend/images/logo.png" alt="" />
+            </a>
+        </div>
+        <ul class="navbar-nav mr-auto d-none d-lg-block"></ul>
+    </nav>
+</div>
---
+++ resources/views/pages/admin/travelpackage/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+    
+@section('title','Halaman Travel Package Admin')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        </div>
+
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Paket Travel</h1>
+            <a href="{{ route('travelpackage.create') }}" class="btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-plus fa-sm text-white-50">Tambah Paket</i>
+            </a>
+        </div>
+        
+        <div class="row">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-primary">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Location</th>
+                                <th>Type</th>
+                                <th>Departure Date</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                                <th colspan="2" class="text-center">#####</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>                            
+                            </tr>                                                    
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+@endsection
