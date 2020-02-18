ee5806e0ff8da2cb78f5f77031a5ba31c7321f64
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-18 14:09:35 +0700

    make:model Gallery

---
+++ app/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gallery extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'travelpackages_id','image'
+    ];
+}
